0a4a9e7aa243a7899613bb3ed9811f24253a9622
TejasBhitle <bhitletejas@gmail.com>
2018-03-27 15:09:59 +0530

    New Dependencies added

--- app/src/main/java/royston_dmello/mirrogh/MainActivity.java
+++ app/src/main/java/royston_dmello/mirrogh/MainActivity.java
@@ -7,15 +7,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import toan.android.floatingactionmenu.FloatingActionButton;
+import toan.android.floatingactionmenu.FloatingActionsMenu;
+
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_IMAGE = "royston_dmello.mirrogh.image_uri";
+    public static final String EXTRA_IMAGE = "extra_image";
     private static int IMG_RESULT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionsMenu floatingactionmenu = findViewById(R.id.floatingactionmenu);
+        FloatingActionButton pickFromGallery = new FloatingActionButton(MainActivity.this);
+        pickFromGallery.setOnClickListener((view) -> {
+
+        });
+        FloatingActionButton pickFromCamera = new FloatingActionButton(MainActivity.this);
+        pickFromCamera.setOnClickListener((view) -> {
+            CropImage.activity(null)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setActivityMenuIconColor(R.color.colorAccent)
+                    .setCropShape(CropImageView.CropShape.RECTANGLE)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+        });
+        floatingactionmenu.addButton(pickFromCamera);
+        floatingactionmenu.addButton(pickFromGallery);
     }
 
     public void clickImage(View view) {
